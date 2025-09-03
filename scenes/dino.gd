47a4731cc09f84985319d0d4a2cc2b7ba738aeab
Sahakanteera <sahakanteera.s@kkumail.com>
2025-09-03 15:48:57 +0700

    -

--- scenes/dino.gd
+++ scenes/dino.gd
@@ -8,18 +8,18 @@ func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if is_on_floor():
 		if not get_parent().game_running:
-			$AnimatedSprite2D2.play("idle")
+			$AnimatedSprite2D.play("idle")
 		else:
 			$RunCol.disabled = false
 			if Input.is_action_pressed("ui_accept"):
 				velocity.y = JUMP_SPEED
 				#$JumpSound.play()
 			elif Input.is_action_pressed("ui_down"):
-				$AnimatedSprite2D2.play("duck")
+				$AnimatedSprite2D.play("duck")
 				$RunCol.disabled = true
 			else:
-				$AnimatedSprite2D2.play("run")
+				$AnimatedSprite2D.play("run")
 	else:
-		$AnimatedSprite2D2.play("jump")
+		$AnimatedSprite2D.play("jump")
 		
 	move_and_slide()
